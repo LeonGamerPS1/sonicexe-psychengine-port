981b1ac30e17e44699aac007009dac7e24b1f5e4
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2021-12-02 05:38:24 -0500

    Upload unfinished source code

---
+++ example_mods/stages/sonicStage.lua
@@ -0,0 +1,73 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('SKY', 'SKY', -222, -16 + 150);
+	setScrollFactor('SKY', 0.9, 0.9);
+	scaleObject('SKY', 1.0, 1.0);
+	
+	makeLuaSprite('FLOOR2', 'FLOOR2', -345, -289 + 170);
+	setScrollFactor('FLOOR2', 0.9, 0.9);
+	scaleObject('FLOOR2', 1.0, 1.0);
+
+	makeLuaSprite('FLOOR1', 'FLOOR1', -297, -246 + 150);
+	setScrollFactor('FLOOR1', 0.9, 0.9);
+	scaleObject('FLOOR1', 1.0, 1.0);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('HILLS', 'HILLS', -264, -156 + 150);
+		setScrollFactor('HILLS', 0.9, 0.9);
+		scaleObject('HILLS', 1.0, 1.0);
+
+		makeLuaSprite('EGGMAN', 'EGGMAN', -218, -219 + 150);
+		setScrollFactor('EGGMAN', 0.9, 0.9);
+		scaleObject('EGGMAN', 1.0, 1.0);
+
+		makeLuaSprite('TAIL', 'TAIL', -119 - 150, -259 + 150);
+		setScrollFactor('TAIL', 0.9, 0.9);
+		scaleObject('TAIL', 1.0, 1.0);
+
+		makeLuaSprite('KNUCKLE', 'KNUCKLE', 185 + 100, -350 + 150);
+		setScrollFactor('KNUCKLE', 0.9, 0.9);
+		scaleObject('KNUCKLE', 1.0, 1.0);
+		
+		makeAnimatedLuaSprite('TailsSpike', 'TailsSpikeAnimated', -100, 50)
+		addAnimationByPrefix('TailsSpike', 'bounce', 'Tails Spike Animated instance 1', 14, true)
+		addLuaSprite('TailsSpike', true)
+		objectPlayAnimation('TailsSpike', 'bounce', true)
+		setScrollFactor('TailsSpike', 0.9, 0.9);
+		scaleObject('TailsSpike', 1.2, 1.2);
+	end
+
+	stepHitFuncs = { --a bunch of timed events, timed to steps
+	[1] = function()
+		makeAnimatedLuaSprite('Jumpscare', 'sonicJUMPSCARE', 0, 370)
+		addAnimationByPrefix('Jumpscare', 'jump', 'sonicSPOOK', 14, true)
+		addLuaSprite('Jumpscare', true)
+		objectPlayAnimation('Jumpscare', 'jump', true)
+		setScrollFactor('Jumpscare', 0.9, 0.9);
+		scaleObject('Jumpscare', 1.1, 1.1);
+		precacheSound('datOneSound');
+	end,
+}
+
+function onStepHit()
+    if stepHitFuncs[curStep] then 
+        stepHitFuncs[curStep]() -- Executes function at curStep in stepHitFuncs
+    end
+	if curStep > 50 then
+		removeLuaSprite('Jumpscare'); --laser dot is gone forever
+	end
+end
+
+	addLuaSprite('SKY', false);
+	addLuaSprite('HILLS', false);
+	addLuaSprite('FLOOR2', false);
+	addLuaSprite('FLOOR1', false);
+	addLuaSprite('EGGMAN', false);
+	addLuaSprite('TAIL', false);
+	addLuaSprite('KNUCKLE', false);
+	addLuaSprite('TailsSpike', false);
+	addLuaSprite('Jumpscare', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
---
+++ example_mods/stages/sonicpixel.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	-- background2 shit
+	makeLuaSprite('GreenHill', 'GreenHill', -428.5 + 50 + 700, -449.35 + 25 + 392 + 105 + 50);
+	setScrollFactor('GreenHill', 0.9, 0.9);
+	scaleObject('GreenHill', 1.4, 1.4);
+
+	addLuaSprite('GreenHill', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
