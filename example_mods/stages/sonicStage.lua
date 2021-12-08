48bc057d6ed61aff959e6fb7bc7c4b31f6980b07
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2021-12-08 15:37:35 -0500

    fixed stage/added note splash

--- example_mods/stages/sonicStage.lua
+++ example_mods/stages/sonicStage.lua
@@ -49,3 +49,4 @@ function onCreate()
    	addLuaSprite('Jumpscare', false);
 	
 	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
