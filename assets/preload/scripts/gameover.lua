d08827c0d60c661c9e07209dc0d87217300db9f0
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-03-17 10:13:14 -0400

    fucking hell

--- assets/preload/scripts/gameover.lua
+++ assets/preload/scripts/gameover.lua
@@ -1,10 +1,8 @@
 function onCreate()
 	if boyfriendName == 'bfFUN' then
 		setPropertyFromClass('GameOverSubstate', 'characterName', 'bfFUN');
-		setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'bunfriend_death_sound');
-		setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameover1');
-	    setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameover2');
-
-		addLuaSprite('topMajins', 100, 100, true)
+		--setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'bunfriend_death_sound');
+		--setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameover1');
+	    --setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameover2');
 	end
 end
