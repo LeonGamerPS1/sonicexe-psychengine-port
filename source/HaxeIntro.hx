e5b80dd8fa65750c2289b52385611f1c3b9c7fb5
JuniorNovoa1 <87715171+JuniorNovoa1@users.noreply.github.com>
2022-03-30 14:14:31 -0400

    caching

--- source/HaxeIntro.hx
+++ source/HaxeIntro.hx
@@ -32,35 +32,45 @@ class HaxeIntro extends MusicBeatState
 		FlxG.mouse.visible = false;
 
 		//main story song shit
-		FlxG.bitmap.add(Paths.image('Phase3Static'));
-		trace('Cached: Phase 3 Static');
+		FlxG.bitmap.add(Paths.image("hitStatic"));
+		trace("Cached: Hit Static");
 
-		FlxG.bitmap.add(Paths.image('hitStatic'));
-		trace('Cached: Hit Static');
+		//Assets for Too Slow
+		FlxG.bitmap.add(Paths.image("sonicJUMPSCARE"));
+		trace("Cached: Too Slow Assets");
 
-		//too slow
-		FlxG.bitmap.add(Paths.image('sonicJUMPSCARE'));
-		trace('Cached: Jumpscare');
+		//Assets for You Can't Run
+		FlxG.bitmap.add(Paths.image("characters/BF", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/Sonic_EXE_Pixel", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/Pixel_gf", "shared"));
+		trace("Cached: You Can't Run Assets");
 
-		//chaos shit
-		FlxG.bitmap.add(Paths.image('characters/Super_BoyFriend_Assets', 'shared'));
-		trace('Cached: Super Boyfriend Assets');
+		//Assets for Triple Trouble
+		FlxG.bitmap.add(Paths.image("characters/BOYFRIEND", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/Tails", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/Beast", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/BFPhase3_Perspective", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/BFPhase3_Perspective_Flipped", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/KnucklesEXE", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/eggman_soul", "shared"));
 
-		FlxG.bitmap.add(Paths.image('characters/Fleetway_Super_Sonic', 'shared'));
-		FlxG.bitmap.add(Paths.image('characters/fleetway1', 'shared'));
-		FlxG.bitmap.add(Paths.image('characters/fleetway2', 'shared'));
-		FlxG.bitmap.add(Paths.image('characters/fleetway3', 'shared'));
-		trace('Cached: Fleetway Assets');
+		FlxG.bitmap.add(Paths.image("Phase3Static"));
+		trace("Cached: Triple Trouble Assets");
 
-		FlxG.bitmap.add(Paths.image('Warning'));
-		trace('Cached: Warning');
-		FlxG.bitmap.add(Paths.image('spacebar_icon'));
-		trace('Cached: Spacebar Icon');
+		//Assets for Chaos
+		FlxG.bitmap.add(Paths.image("characters/Super_BoyFriend_Assets", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/Fleetway_Super_Sonic", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/fleetway1", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/fleetway2", "shared"));
+		FlxG.bitmap.add(Paths.image("characters/fleetway3", "shared"));
+		FlxG.bitmap.add(Paths.image("Warning"));
+		FlxG.bitmap.add(Paths.image("spacebar_icon"));
+		trace("Cached: Chaos Assets");
 
-		trace('Done Caching!'); //this caching shit isnt from the original mod except for the chaos cache shit but yea i hate lag.
+		trace("Done Caching!"); //this caching shit isnt from the original mod except for the chaos cache shit but yea i hate lag.
 
 		#if windows
-		video.playMP4(Paths.video('HaxeFlixelIntro'));
+		video.playMP4(Paths.video("HaxeFlixelIntro"));
 		video.finishCallback = function()
 		{
 			MusicBeatState.switchState(new TitleState());
